2c458d5a1e0f5080058d134980e03257de6d0680
0kso <okso@boa>
2012-07-31 21:02:34 +0200

    Added decorator @peachpy.expose

--- peachpy/__init__.py
+++ peachpy/__init__.py
@@ -2,7 +2,13 @@
 import sys
 
 def quickstart(app):
+	'App launcher'
 	if len(sys.argv) <= 1:
 		print app.index()
 	else:
 		print app.__getattr__(sys.argv[1])
+
+def expose(method):
+	'@peachpy.expose decorator'
+	method.exposed = True
+	return method
